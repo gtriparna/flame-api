e8f757dac8edcabb1f5f2e1d0d4024665c026695
Triparna de Vreede <49166756+gtriparna@users.noreply.github.com>
2025-04-12 02:52:27 -0400

    Update minimal Flask app

--- minimal.py
+++ minimal.py
@@ -11,6 +11,8 @@ def hello():
 def test():
     return jsonify({"test": "success"})
 
+# Add any other routes before this line
+# The if __name__ block should only contain run configuration
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
